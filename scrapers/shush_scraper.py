50911129f2ac3d81ade7223967f02c9892082970
Tracy Norris <tknorris@gmail.com>
2014-09-23 16:56:44 -0400

    pass video object down into get_ep_url

--- scrapers/shush_scraper.py
+++ scrapers/shush_scraper.py
@@ -141,10 +141,10 @@ class Shush_Scraper(scraper.Scraper):
                 results.append(result)
         return results
     
-    def _get_episode_url(self, show_url, season, episode, ep_title):
-        episode_pattern = '<div class="list2"><a\s+href="([^"]+)"[^<]+Season\s+%s\s+Episode:\s+%s' % (season, episode)
+    def _get_episode_url(self, show_url, video):
+        episode_pattern = '<div class="list2"><a\s+href="([^"]+)"[^<]+Season\s+%s\s+Episode:\s+%s' % (video.season, video.episode)
         title_pattern= '<div class="list2"><a\s+href="([^"]+)"[^<]+Season\s+1\s+Episode:\s+\d+\s+-\s+([^<]+)'
-        url = super(Shush_Scraper, self)._default_get_episode_url(show_url, season, episode, ep_title, episode_pattern, title_pattern)
+        url = super(Shush_Scraper, self)._default_get_episode_url(show_url, video, episode_pattern, title_pattern)
         if url and not url.startswith('/'): url = '/' + url
         return url
         
